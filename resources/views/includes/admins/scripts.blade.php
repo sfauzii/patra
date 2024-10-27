8fdd9233ed23451e8fe40b567d7b48f4f3f5acc3
sfauzii <sjahrulfauzii@gmail.com>
2024-10-27 15:23:50 +0700

    refactor(templating): templating untuk bagian backend dashbord

---
+++ resources/views/includes/admins/scripts.blade.php
@@ -0,0 +1,11 @@
+<script src="{{ url('backend/assets/static/js/components/dark.js') }}"></script>
+<script src="{{ url('backend/assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+
+
+<script src="{{ url('backend/assets/compiled/js/app.js') }}"></script>
+
+
+
+<!-- Need: Apexcharts -->
+<script src="{{ url('backend/assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ url('backend/assets/static/js/pages/dashboard.js') }}"></script>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dashboard - Mazer Admin Dashboard</title>
+
+        @include('includes.admins.style')
+
+    </head>
+
+    <body>
+        <script src="assets/static/js/initTheme.js"></script>
+        <div id="app">
+
+            @include('includes.admins.sidebar')
+
+
+            <div id="main">
+
+                @yield('content')
+
+                @include('includes.admins.footer')
+            </div>
+        </div>
+
+        @include('includes.admins.scripts')
+
+    </body>
+
+</html>
