d2120d1ba211b39213ec701acdeb81d540c3bbb0
sfauzii <sjahrulfauzii@gmail.com>
2024-11-09 12:44:35 +0700

    fix(items migrations): fixed tipe data description menjadi longtext

--- database/migrations/2024_11_05_134404_change_field_description_items_table.php
+++ database/migrations/2024_11_05_134404_change_field_description_items_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->longText('description');
+            $table->longText('description')->change();
         });
     }
 
