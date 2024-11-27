1e368b23e4b1508fe0ea9edac8a7b0c953e868f6
sfauzii <sjahrulfauzii@gmail.com>
2024-11-27 19:08:09 +0700

    refactor(cars): display rating di card cars

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -10,10 +10,15 @@ class CarsController extends Controller
     public function cars()
     {
 
-        $items =  Item::with(['type', 'brand'])
+        $items =  Item::with(['type', 'brand', 'reviews'])
             ->available()
             ->where('is_available', true)
-            ->get();
+            ->get()
+            ->map(function ($item) {
+                $item->avg_rating = $item->reviews->avg('rating') ?? 0;
+                $item->review_count = $item->reviews->count();
+                return $item;
+            });
 
         return view('cars', [
             'items' => $items,
